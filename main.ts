8086ae7a8085881acf3c183944d19898ae7971d2
Nico-gnt <68139398+Nico-gnt@users.noreply.github.com>
2020-07-13 11:17:38 +1100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,9 @@
-basic.showString("Bonjour")
-basic.showNumber(10853)
-basic.showArrow(ArrowNames.South)
-basic.showIcon(IconNames.Happy)
+for (let index = 0; index < 2; index++) {
+    basic.showString("Bonjour")
+    basic.showNumber(10853)
+    basic.showArrow(ArrowNames.South)
+    basic.showIcon(IconNames.Happy)
+}
 basic.forever(function () {
     basic.pause(500)
 })
